d71e62ad4b6c0a982df296d67d08dccea6f6cc4a
Tunay Bektas <what05@web.de>
2022-12-06 13:02:04 +0100

    Top Bar added TopBar.js and styled it installed React-Icons and added the icons

---
+++ src/components/TopBar.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { AiFillPhone, AiOutlineClockCircle } from "react-icons/ai";
+import { BsChatSquareDots } from "react-icons/bs";
+
+function TopBar() {
+  return (
+    <div className="flex justify-between items-center px-4 py-2">
+      <div className="flex items-center">
+        <BsChatSquareDots
+          size={30}
+          className="text-[var(--primary-dark)] mr-2"
+        />
+        <h1 className="text-xl font-bold text-gray-700">VACATIONS</h1>
+      </div>
+      <div className="flex">
+        <div className="hidden md:flex items-center px-6">
+          <AiOutlineClockCircle
+            size={20}
+            className="mr-2 text-[var(--primary-dark)]"
+          />
+          <p className="text-sm text-gray-700">08:00 - 18:00</p>
+        </div>
+        <div className="hidden md:flex items-center px-6">
+          <AiFillPhone size={20} className="mr-2 text-[var(--primary-dark)]" />
+          <p className="text-sm text-gray-700"> 0241 5077-145320</p>
+        </div>
+        <button>Kostenloses Angebot anfordern</button>
+      </div>
+    </div>
+  );
+}
+
+export default TopBar;
