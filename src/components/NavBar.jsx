3c97eb2432fe3c3c249d4f65eba9f57a2314fbad
Tunay Bektas <what05@web.de>
2022-12-07 11:37:51 +0100

    Created NavBar.js added different social network icons and a hamburger Menu for mobile

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,69 @@
+import { useState, useEffect } from "react";
+import {
+  FaBars,
+  FaFacebookF,
+  FaTwitter,
+  FaGooglePlusG,
+  FaInstagram,
+} from "react-icons/fa";
+import { BsChatSquareDots } from "react-icons/bs";
+
+function NavBar() {
+  const [nav, setNav] = useState(false);
+
+  const handleNav = () => {
+    setNav(!nav);
+  };
+  return (
+    <div className="w-full min-h-[50px] flex justify-between items-center absolute z-10 text-white bg-gray-700/80">
+      <ul className="hidden sm:flex px-4">
+        <li>
+          <a href="/">Home</a>
+        </li>
+        <li>
+          <a href="#galerie">Galerie</a>
+        </li>
+        <li>
+          <a href="#angebote">Angebote</a>
+        </li>
+        <li>
+          <a href="#kontakt">Kontakt</a>
+        </li>
+      </ul>
+      <div className="flex justify-between">
+        <FaFacebookF className="mx-4" />
+        <FaTwitter className="mx-4" />
+        <FaInstagram className="mx-4" />
+        <FaGooglePlusG className="mx-4" />
+      </div>
+      <div onClick={handleNav} className="sm:hidden z-10">
+        <FaBars size={20} className="mr-4 cursor-pointer" />
+      </div>
+      <div
+        onClick={handleNav}
+        className={
+          nav
+            ? "overflow-y-hidden md:hidden ease-in duration-300 absolute text-gray-300 left-0 top-0 w-full h-screen bg-black/90 px-4 py-7 flex flex-col"
+            : "absolute top-0 h-screen left-[-100%] ease-in duration-500"
+        }
+      >
+        <ul className="h-full w-full text-center pt-12">
+          <li className="text-2xl py-8">
+            <a href="/">Home</a>
+          </li>
+          <li className="text-2xl py-8">
+            <a href="#galerie">Galerie</a>
+          </li>
+          <li className="text-2xl py-8">
+            <a href="#angebote">Angebote</a>
+          </li>
+          <li className="text-2xl py-8">
+            <a href="#kontakt">Kontakt</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default NavBar;
--- src/components/TopBar.jsx
+++ src/components/TopBar.jsx
@@ -10,7 +10,7 @@ function TopBar() {
           size={30}
           className="text-[var(--primary-dark)] mr-2"
         />
-        <h1 className="text-xl font-bold text-gray-700">VACATIONS</h1>
+        <h1 className="text-xl font-bold text-gray-700">REACT TRAVEL</h1>
       </div>
       <div className="flex">
         <div className="hidden md:flex items-center px-6">
@@ -22,7 +22,7 @@ function TopBar() {
         </div>
         <div className="hidden md:flex items-center px-6">
           <AiFillPhone size={20} className="mr-2 text-[var(--primary-dark)]" />
-          <p className="text-sm text-gray-700"> 0241 5077-145320</p>
+          <p className="text-sm text-gray-700"> 0000 000-0000</p>
         </div>
         <button>Kostenloses Angebot anfordern</button>
       </div>
