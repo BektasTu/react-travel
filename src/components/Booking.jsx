699e5991536c259c8d831c7d85034f80f2150146
Tunay Bektas <what05@web.de>
2022-12-07 14:06:19 +0100

    Styled the booking form

--- src/components/Booking.jsx
+++ src/components/Booking.jsx
@@ -2,11 +2,11 @@ import React from "react";
 
 function Booking() {
   return (
-    <div id="angebote">
-      <form>
-        <div>
+    <div id="angebote" className="max-w-[1140px] m-auto w-full p-4">
+      <form className="lg:flex lg:justify-between w-full items-center">
+        <div className="flex flex-col my-2 py-2">
           <label>Reiseziel</label>
-          <select name="" id="">
+          <select className="lg:w-[300px] md:w-full border rounded-md p-2">
             <option>Curaçao</option>
             <option>Seychellen</option>
             <option>Turks- und Caicosinseln</option>
@@ -19,19 +19,19 @@ function Booking() {
             <option>Mexiko</option>
           </select>
         </div>
-        <div>
-          <div>
+        <div className="flex w-full">
+          <div className="flex flex-col w-full lg:max-w-[250px] my-2 p-2">
             <label>Anreisedatum</label>
-            <input type="date" />
+            <input className="border rounded-md p-2" type="date" />
           </div>
-          <div>
+          <div className="flex flex-col w-full lg:max-w-[250px] my-2 p-2">
             <label>Abreisedatum</label>
-            <input type="date" />
+            <input className="border rounded-md p-2" type="date" />
           </div>
         </div>
-        <div>
+        <div className="flex flex-col my-2 py-2 w-full">
           <label>Suche</label>
-          <button>Preise & Verfügbarkeit</button>
+          <button className="w-full">Preise & Verfügbarkeit</button>
         </div>
       </form>
     </div>
