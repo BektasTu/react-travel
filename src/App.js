f7219ff0556fb1dd72740ee3f93b899c2f5c22bd
Tunay Bektas <what05@web.de>
2022-12-07 14:48:24 +0100

    Created Gallery.jsx added images and stleyed them

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import Activities from "./components/Activities";
 import Background from "./components/Background";
 import Booking from "./components/Booking";
+import Gallery from "./components/Gallery";
 import NavBar from "./components/NavBar";
 import TopBar from "./components/TopBar";
 
@@ -13,6 +14,7 @@ function App() {
       <Background />
       <Activities />
       <Booking />
+      <Gallery />
     </div>
   );
 }
---
+++ src/components/Gallery.jsx
@@ -0,0 +1,93 @@
+import React from "react";
+
+const Gallery = () => {
+  return (
+    <div id="galerie" className="max-w-[1140px] w-full px-4 py-16">
+      <h2 className="text-center text-gray-800 p-4">Galerie</h2>
+      <h3 className="text-center text-gray-700 p-1">
+        Wohin geht's als nächstes?
+      </h3>
+      <div className="grid sm:grid-cols-5 gap-4">
+        <div className="sm:col-span-3 col-span-2 row-span-2">
+          <img
+            className="w-full h-full object-cover"
+            src="https://images.unsplash.com/photo-1590523741831-ab7e8b8f9c7f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1374&q=80"
+            alt="/"
+          />
+        </div>
+        <div>
+          <img
+            className="w-full h-full object-cover"
+            src="https://images.unsplash.com/photo-1626956291772-3aa243614fd0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1374&q=80"
+            alt="/"
+          />
+        </div>
+        <div>
+          <img
+            className="w-full h-full object-cover"
+            src="https://images.unsplash.com/photo-1557409518-691ebcd96038?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
+            alt="/"
+          />
+        </div>
+        <div>
+          <img
+            className="w-full h-full object-cover"
+            src="https://images.unsplash.com/photo-1543783207-ec64e4d95325?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
+            alt="/"
+          />
+        </div>
+        <div>
+          <img
+            className="w-full h-full object-cover"
+            src="https://images.unsplash.com/photo-1518098268026-4e89f1a2cd8e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1374&q=80"
+            alt="/"
+          />
+        </div>
+        <div>
+          <img
+            className="w-full h-full object-cover"
+            src="https://images.unsplash.com/photo-1607585011081-241d2bacb7de?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
+            alt="/"
+          />
+        </div>
+        <div>
+          <img
+            className="w-full h-full object-cover"
+            src="https://images.unsplash.com/photo-1509316785289-025f5b846b35?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1476&q=80"
+            alt="/"
+          />
+        </div>
+        <div>
+          <img
+            className="w-full h-full object-cover"
+            src="https://images.unsplash.com/photo-1562823083-3f86817ec7a2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
+            alt="/"
+          />
+        </div>
+        <div>
+          <img
+            className="w-full h-full object-cover"
+            src="https://images.unsplash.com/photo-1554254648-2d58a1bc3fd5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1471&q=80"
+            alt="/"
+          />
+        </div>
+        <div>
+          <img
+            className="w-full h-full object-cover"
+            src="https://images.unsplash.com/photo-1506894740382-be98b67900b5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1509&q=80"
+            alt="/"
+          />
+        </div>
+        <div>
+          <img
+            className="w-full h-full object-cover"
+            src="https://images.unsplash.com/photo-1492963060540-65e3aae73127?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1412&q=80"
+            alt="/"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Gallery;
